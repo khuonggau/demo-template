33144620750e2205284d6e89b4d53d6e5197af0f
khuonggau <khuongbit7@gmail.com>
2015-05-20 15:20:29 +0700

    Hover menu with Jquery

---
+++ _/components/js/_myscript.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	// make menus drop automatically
+	$('ul.nav li.dropdown').hover(function(){
+		$('.dropdown-menu',this).fadeIn();
+	}, function(){
+		$('.dropdown-menu',this).fadeOut('fast');
+	});	// hover
+
+});	// jquery is loaded
