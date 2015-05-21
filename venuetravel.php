7e89eafc1e56105bed77b092cff8060b8c2c1326
khuonggau <khuongbit7@gmail.com>
2015-05-21 15:40:34 +0700

    reusing photo component

--- venuetravel.php
+++ venuetravel.php
@@ -12,10 +12,13 @@
       <div class="content row">
         <?php include "_/components/php/header.php"; ?>
         <section class="main col col-lg-8">
-          
+          <?php include "_/components/php/article-venues.php"; ?>
         </section><!-- main -->
         <section class="sidebar col col-lg-4">
-          
+          <?php include "_/components/php/aside-register.php"; ?>
+          <?php include "_/components/php/aside-lastyear.php"; ?>
+          <?php include "_/components/php/aside-abouttheartists.php"; ?>
+          <?php include "_/components/php/aside-accordion.php"; ?>
         </section><!-- sidebar -->
       </div><!-- content -->
       <?php include "_/components/php/footer.php"; ?>
