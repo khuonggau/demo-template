6b15469c8df2066ee4d5209284276e77cbb50205
khuonggau <khuongbit7@gmail.com>
2015-05-22 14:49:28 +0700

    Styling ScrollSpy component

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,5 +1,3 @@
-<div class="content row">
-	<div class="col-lg-12">
 		<header class="clearfix">
 			<section id="branding">
 				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="logo"></a>
@@ -40,5 +38,3 @@
 				</div><!-- modal-body -->
 			</section><!-- modal -->
 		</header><!-- header -->
-	</div><!-- column -->
-</div><!-- content -->
